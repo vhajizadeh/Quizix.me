459ea28e32d4b1beb0ca66209ed8596afeec5615
Arif Khan <arifkpi@gmail.com>
2018-01-28 00:42:59 +0600

    Quick Quiz Mode Added

--- resources/views/admin/settings.blade.php
+++ resources/views/admin/settings.blade.php
@@ -58,7 +58,7 @@
 	</div>   --}}
 
 	<div class="row">
-        <div class="col-xs-6">
+        <div class="col-xs-12">
             <div class="panel">
                 <header class="panel-heading">
                     API URLs
